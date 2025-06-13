7af3fb38a8d8fdca4cd52f2a87be337a94d3590b
tuan <tuan@aviansolutions.aero>
2025-06-13 11:32:35 +0700

    add command deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
-
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   target: "web",
@@ -51,6 +51,17 @@ module.exports = {
       process: {
         env: {},
       },
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: path.join(__dirname, "public"),
+          to: path.join(__dirname, "dist"),
+          globOptions: {
+            ignore: ["**/index.html"],
+          },
+        },
+      ],
     }),
   ],
 };
